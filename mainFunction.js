fa2e3896260d74fd91dcb48309ab48a7b18b5fdc
Srejon Sharma <srejonboss2@gmail.com>
2025-02-26 19:57:26 +0600

    function2 & logic added

--- mainFunction.js
+++ mainFunction.js
@@ -20,3 +20,32 @@ function userDeposit(){
     previousTotalText.innerText = newTotal;
 
 }
+
+function userWithdrawal(){
+    const withdrawInput = document.getElementById("amount");
+    const withdrawValue = withdrawInput.value;
+    const withdrawIt = Number(withdrawValue);
+
+
+    //done withdraw calculation
+    const previousWithdrawText = document.getElementById("withdraw");
+    const previousWithdrawValue = previousWithdrawText.innerText;
+    const previousWithdraw = Number(previousWithdrawValue);
+    
+    
+
+    //done Total withdraw
+    const previousTotalText = document.getElementById("total");
+    const previousTotalValue = previousTotalText.innerText;
+    const previousTotal = Number(previousTotalValue);
+
+    if(withdrawIt > previousTotal){
+        alert('Not enough money!');
+        return;
+    }else {
+        const newWithdraw = withdrawIt + previousWithdraw;
+        previousWithdrawText.innerText = newWithdraw;
+        const newTotal =  previousTotal - withdrawIt;
+        previousTotalText.innerText = newTotal;
+    }
+}
